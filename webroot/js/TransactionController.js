7ea77bd37c0813467f78e519120338ecbf760ad1
Jeffrey Young <jeffreyyoung@dhcp-v880-11a-00327.1Xwireless.unc.edu>
2015-12-03 12:21:00 -0500

    finished transaction table

--- webroot/js/TransactionController.js
+++ webroot/js/TransactionController.js
@@ -18,56 +18,27 @@ $(document).ready(function() {
         {type: "GET",
             dataType: "json",
             success: function(transactions){
-                var companies = Array();
                 for(var i = 0; i < transactions.length; i++){
-                    var name = transactions[i].companyName;
-                    if(companies.indexOf(name) == -1){
-                        companies.push(name);
-                    }
-                }
-
-                var portfolioSummary = Array();
-
-                for(i = 0; i < companies.length; i++){
-                    var totalShares = 0;
-                    var companyTransactions = transactions.filter(function(transaction){
-                        return (transaction.companyName == companies[i]);
-                    });
-
-                    for(var j = 0; j < companyTransactions.length; j++){
-                        if(companyTransactions[j].type == '1'){
-                            totalShares += parseFloat(companyTransactions[j].shares);
-                        } else {
-                            totalShares -= parseFloat(companyTransactions[j].shares);
-                        }
-                    }
-                    portfolioSummary.push({
-                        "company" : companyTransactions[i],
-                        "totalShares" : totalShares
-                    })
-                }
-
-                for(i = 0; i < portfolioSummary.length; i++){
-                    (function(summary){
-                        $.ajax("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22"+summary.company.symbol+"%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",{
-                            dataType: "json",
-                            data: { symbol : summary.company.symbol},
-                            success: function(quote){
-                                $info = quote.query.results.quote;
-                                var companyName = '<td class="companyName">'+ summary.company.companyName +'</div>';
-                                var totalShares = '<td class="totalShares">'+ round(summary.totalShares, 2) +'</td>';
-                                var open = '<td class="openPrice">'+ $info.DaysRange +'</td>';
-                                var lastPrice = '<td class="lastPrice">'+ round($info.LastTradePriceOnly, 2) +'</td>';
-                                var currentValue = '<td class="currentValue">' + round($info.LastTradePriceOnly * summary.totalShares, 2) + '</td>';
-                                var commission = '<td class="commission">' + "test" + '</td>';
-                                var notes = '<td class="notes">' + "test" + '</td>';
-                                $("tbody").append('<tr class="summaryContainer">' + companyName + totalShares + open + lastPrice + currentValue + commission + notes + "</tr>");
-                            }
-                         })
-                    }(portfolioSummary[i]));
+                	var actions = '<td class="action">' + "test" + '</td>';
+                	var company = '<td class="company">' + transactions[i].companyName + '</td>';
+                	var symbol = '<td class="symbol">' + transactions[i].symbol + '</td>';
+                	var temp = transactions[i].type;
+                	var type;
+                	if (temp == 1){
+                		type = '<td class="type">' + "Buy" + '</td>';
+                	}
+                	else {
+                		type = '<td class="type">' + "Sell" + '</td>';
+                	}
+                	var date = '<td class="date">' + transactions[i].date + '</td>';
+                	var shares = '<td class="shares">' + round(transactions[i].shares, 2) + '</td>';
+                	var price = '<td class="price">' + transactions[i].price + '</td>';
+					var commission = '<td class="commission">' + transactions[i].commission + '</td>';
+                	var notes = '<td class="notes">' + transactions[i].notes + '</td>';
+                	$("tbody").append('<tr class="summaryContainer">' + actions + company + symbol + type + date + shares + price + commission + notes + "</tr>");
                 }
             }
-        });
+        })
 });
 
 $(document).ajaxStop(function(){
